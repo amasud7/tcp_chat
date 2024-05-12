353aae480e5835e2551954fc3d6b1b644ed71184
Ayad Masud <ayad.r.masud@gmail.com>
2024-05-11 22:35:05 -0500

    updated server with multi threading for handling clients

--- server.py
+++ server.py
@@ -9,21 +9,55 @@ import threading
 HOST = socket.gethostbyname(socket.gethostname())
 port = 5050
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # with block so server closes by itself at the end
-    # binding server to host and port
-    # listening for potential client connections
-    s.bind((HOST, port))
-    s.listen()
+# initializing socket object (server socket)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, port))
+server.listen()
 
-    conn, addr = s.accept() # .accept() returns connection and address of client for connection
-                                    # also blocks rest of the code until connection is found
-    with conn: # conn is a socket object 
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+
+clients = []
+names = []
+
+def broadcast(msg):
+    for client in clients: # send messages to each client in list 
+        client.send(msg) 
+
+# when each client connects starts receiving messages from them
+def handle_client(client):
+    while True: # infinite loop to continue getting messages from client
+        try:
+            message = client.recv(1024) # receive messages from client
+            broadcast(message) # update all other users with sent messages
+        except: # if there is error with client 
+            index = clients.index(client) # remove client from lists and close connection 
+            clients.remove(index)
+            names.remove(index)
+            broadcast(f"{names[index]} has left the chat.")
+            client.close()
+            break
+
+def main():
+    # searching for connecting clients
+    while True:
+        client, addr = server.accept() # client connected and its address is stored in the variables
+
+    # asking user for nickname
+        client.send("NICK".encode('utf-8'))
+        name = client.recv(1064) # dont understand why this is client and not server
+        clients.append(client)
+        names.append(name)
+        
+    # Broadcasting name joined
+        broadcast(f"{name} has joined the chat".encode('utf-8'))
+        client.send("You have connected to server".encode('utf-8')) # telling user they have connected
+
+    # starting a new thread for every connection
+        thread = threading.Thread(target=handle_client, args=client)
+        thread.start()
+
+
+print("Server is listening...")
+main()
 
 
 
