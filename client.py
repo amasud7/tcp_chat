d8118d783f4e2697701d14bace1d6283743cf325
Ayad Masud <ayad.r.masud@gmail.com>
2024-05-12 12:48:17 -0500

    updated client

--- client.py
+++ client.py
@@ -1,13 +1,39 @@
 import socket
 import threading
 
+# asking for name 
+name = input("Please enter nickname: ")
+
+# getting host name and port number
 HOST = socket.gethostbyname(socket.gethostname())
 port = 5050
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, port))
-    s.send(b"Hello world")
-    data = s.recv(1024)
+# creating socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connecting client socket to server
+client.connect((HOST, port))
 
-    print(f"Server reply: {data}")
+def receive():
+    while True:
+        try:
+            msg = client.recv(1024).decode('utf-8') # messages coming directly from server not other client
+            if msg == "NICK":
+                client.send(name.encode('utf-8'))
+            else:
+                print(msg)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
 
+def write():
+    while True:
+        msg = f"{name}: {input('')}"
+        client.send(msg.encode('utf-8'))
+
+# multi threading for managing receiving and writing messages at the same time 
+rec = threading.Thread(target=receive)
+rec.start()
+
+wrt = threading.Thread(target=write)
+wrt.start()
